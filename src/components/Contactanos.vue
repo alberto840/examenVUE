237cb408ff2eee2cb9e889687b45cff41845ef21
carlaval655 <carla.val655@gmail.com>
2022-04-29 07:07:53 -0400

    Contactanos terminado

--- src/components/Contactanos.vue
+++ src/components/Contactanos.vue
@@ -1,28 +1,102 @@
+<style>
+.header {
+    color: #36A0FF;
+    font-size: 27px;
+    padding: 10px;
+}
+
+.bigicon {
+    font-size: 35px;
+    color: #36A0FF;
+}
+</style>
 <template>
-    <div class="parent">
-        <div class="child position-absolute start-50"><iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d30172.927342492836!2d-65.25918076931153!3d-19.036639000000005!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x93fbc8cef38e6c77%3A0xb405926e8bb6321a!2sMercado%20Campesino%20Sucre!5e0!3m2!1ses!2sbo!4v1651118556860!5m2!1ses!2sbo" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></div>
-        <div class="child position-absolute end-50">
-            <h1>Telf: 214124124</h1>
+<center>
+  <div class="container" style="width:200vw; background-color:#C7F464; padding:2vw;">
+        <div class="col-md-20">
+            <div class="well well-sm">
+                <form class="form-horizontal" method="post">
+                    <fieldset>
+                        <center><legend class="text-center header" style="color:white;">CONTACTANOS</legend></center>
+
+                        <div class="form-group" style="margin:1vw;">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="fname" name="name" type="text" placeholder="NOMBRE(S)" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group" style="margin:1vw;">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="lname" name="name" type="text" placeholder="APELLIDO(S)" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group" style="margin:1vw;">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-envelope-o bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="email" name="email" type="text" placeholder="CORREO ELECTRONICO" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group" style="margin:1vw;">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="phone" name="phone" type="text" placeholder="CELULAR" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group" style="margin:2vw;">
+                            <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-pencil-square-o bigicon"></i></span>
+                            <div class="col-md-8">
+                                <textarea class="form-control" id="message" name="message" placeholder="Ingresa tu mensaje en este espacio. Nos comunicaremos contigo lo mas pronto posible" rows="7"></textarea>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-12 text-center">
+                                <button id="enviar" class="btn btn-primary btn-lg" style="background-color: #C44D58;" disabled>Enviar</button>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+</div>
+</center>
+        <div class="child position-absolute end-50" style="background-color:#4ECDC4">
+            <center>
+              <h1>Telf: 214124124</h1>
             <h1>Telf: 234525526</h1>
             <h1>Direccion: Los andes numero 500, calle 54</h1>
             <h1>nombre de encargado: Ignacion Muñoz Quispe</h1>
+            </center>
         </div>
-    </div>
-    
+        <div class="child position-absolute start-50 " style="background-color:#FF6B6B">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d30172.927342492836!2d-65.25918076931153!3d-19.036639000000005!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x93fbc8cef38e6c77%3A0xb405926e8bb6321a!2sMercado%20Campesino%20Sucre!5e0!3m2!1ses!2sbo!4v1651118556860!5m2!1ses!2sbo" width="600" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
 </template>
-
-<style>
-.parent {
-  border: 1px solid black;
-  margin: 1rem;
-  padding: 2rem 2rem;
-  text-align: center;
-  color: azure;
-}
-.child {
-  display: inline-block;
-  border: 1px solid #0000;
-  padding: 1rem 1rem;
-  vertical-align: middle;
-}
-</style>
+<script type="text/javascript">
+function enviar(){
+    alert("Se envio el formulario correctamente. Pronto estaremos en contacto contigo");
+  }
+export default {
+    data() {
+      return {
+        types: [
+          'text',
+          'number',
+          'email',
+          'password',
+          'search',
+          'url',
+          'tel',
+          'date',
+          'time',
+          'range',
+          'color'
+        ]
+      }
+    }
+  };
+</script>
