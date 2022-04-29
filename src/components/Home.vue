d3955d58a8b67252c7ee95615d3e698e7c6935af
Alberto840 <alberto.flores@ucb.edu.bo>
2022-04-29 00:35:06 -0400

    actualiza la web home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,92 +1,30 @@
 <template>
+        <!--Titulares-->
+        <div class="child" style="width: 100%;"><h1 class="titular">Los destacados:</h1></div>
+
+        <!--Bloque1-->
         <div class="child" style="width: 33%">
-          <img src="https://vidayfuturoorgpe.files.wordpress.com/2016/07/fin-del-mundo1.jpg" alt="foto1">
+          <img src="https://cdn.techtitute.com/techtitute-blog/2021/04/Periodismo-de-precision-1.jpg" alt="foto1" style="width: 420px;">
         </div>
         <div  class="child" style="width: 33%">
-            <div id="myCarousel" class="carousel slide" data-ride="carousel">
-        <!-- Indicators -->
-            <ol class="carousel-indicators">
-              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-              <li data-target="#myCarousel" data-slide-to="1"></li>
-              <li data-target="#myCarousel" data-slide-to="2"></li>
-            </ol>
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner">
-        <div class="item active">
-          <img src="https://imborrable.com/wp-content/uploads/2021/04/fotos-gratis-de-stock-1.jpg" alt="Los Angeles" style="width: 600px;">
+          <h1>Tension en china con estudiantes.</h1>
+          <p>La fotografía es testigo de la historia. Al hombre que detuvo por unos segundos una columna de tanques en la plaza de Tiannanen en Pequín se le llegó a llamar tank man (hombre tanque) o rebelde desconocido. Supuestamente era uno de los millares de estudiantes que se manifestó entre abril y junio de 1989 contra el gobierno de la República Popular China y cuyo movimiento acabó en una sangrienta represión por parte del Ejército Popular de Liberación. En abril de 1998 la revista Time consideró al rebelde desconocido como una de las personas más influyentes del siglo XX. Nada se sabe de él, muchos creen que fue ejecutado por su osadía, aunque ninguna información ha confirmado nunca esta teoría. Sea como fuere, jamás ha salido a la luz en busca de reconocimiento. Su gesto, en cambio, difícilmente será olvidado.</p>
         </div>
-
-        <div class="item">
-         <img src="https://img.freepik.com/foto-gratis/disparo-gran-angular-solo-arbol-que-crece-cielo-nublado-puesta-sol-rodeada-cesped_181624-22807.jpg?w=2000" alt="Chicago" style="width: 600px;">
-        </div>
-
-        <div class="item">
-         <img src="https://productoraudiovisualbarcelona.com/wp-content/uploads/2018/02/fotografia-eventos-exposiciones-785x510.jpg" alt="New York" style="width: 600px;">
-        </div>
-        </div>
-
-  <!-- Left and right controls -->
-  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#myCarousel" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right"></span>
-    <span class="sr-only">Next</span>
-  </a>
-            </div>
-            
-        </div>
-        <div  class="child" style="width: 33%">
-          <p>haaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaola</p>
-        </div>
-
         <div class="child" style="width: 33%">
-          <h1>Las mejores fotos de nuestra compañia</h1>
-        <br><br><br>
-        <h2>Nuestra historia</h2>
-        <br><br>
-        <p>Se llama fotografía a una técnica y a una forma de arte que consisten en capturar imágenes empleando para ello la luz, proyectándola y fijándola en forma de imágenes sobre un medio sensible (físico o digital).<br> La fotografía toda se basa en el mismo principio de la “cámara oscura”, un instrumento óptico que consiste en un compartimiento totalmente oscuro dotado de un agujero pequeño en uno de sus extremos, por el cual ingresa la luz y proyecta sobre el fondo oscurecido las imágenes de lo que ocurra afuera del compartimiento, aunque invertidas.
-En el caso de las cámaras fotográficas, el principio es exactamente el mismo, excepto que están dotadas de lentes para afinar el foco de lo proyectado, espejos para reinvertir la imagen proyectada y por último una cinta fotosensible (o un sensor digital semejante), que capta la imagen y la guarda, para poder luego revelarla o visualizarla digitalmente.
-Las imágenes obtenidas así también se denominan fotografías o fotos, y son el resultado de décadas de perfeccionamiento de la técnica y de los materiales fotosensibles, hasta lograr la calidad óptica de las cámaras modernas.<br> Además, esta tecnología permitió el desarrollo y perfeccionamiento de otras semejantes, incluida la cinematografía.</p>
+          <img src="https://escuelaergon.com/wp-content/uploads/Tipos-de-periodismo-1080x675.jpg" alt="foto2" style="width: 420px;">
+        </div>
+        
+        <!--Bloque2-->
+        <div  class="child" style="width: 33%">
+          <h1>Conflictos en la capital.</h1>
+          <p>La fotografía es testigo de la historia. Al hombre que detuvo por unos segundos una columna de tanques en la plaza de Tiannanen en Pequín se le llegó a llamar tank man (hombre tanque) o rebelde desconocido. Supuestamente era uno de los millares de estudiantes que se manifestó entre abril y junio de 1989 contra el gobierno de la República Popular China y cuyo movimiento acabó en una sangrienta represión por parte del Ejército Popular de Liberación. En abril de 1998 la revista Time consideró al rebelde desconocido como una de las personas más influyentes del siglo XX. Nada se sabe de él, muchos creen que fue ejecutado por su osadía, aunque ninguna información ha confirmado nunca esta teoría. Sea como fuere, jamás ha salido a la luz en busca de reconocimiento. Su gesto, en cambio, difícilmente será olvidado.</p>
+        </div>
+        <div class="child" style="width: 33%">
+          <img src="https://www.famouspictures.org/wp-content/uploads/2013/05/Tank-Man1.jpg" alt="foto2" style="width: 420px;">
         </div>
         <div  class="child" style="width: 33%">
-            <div id="myCarousel" class="carousel slide" data-ride="carousel">
-        <!-- Indicators -->
-            <ol class="carousel-indicators">
-              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-              <li data-target="#myCarousel" data-slide-to="1"></li>
-              <li data-target="#myCarousel" data-slide-to="2"></li>
-            </ol>
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner">
-        <div class="item active">
-          <img src="https://imborrable.com/wp-content/uploads/2021/04/fotos-gratis-de-stock-1.jpg" alt="Los Angeles" style="width: 600px;">
-        </div>
-
-        <div class="item">
-         <img src="https://img.freepik.com/foto-gratis/disparo-gran-angular-solo-arbol-que-crece-cielo-nublado-puesta-sol-rodeada-cesped_181624-22807.jpg?w=2000" alt="Chicago" style="width: 600px;">
-        </div>
-
-        <div class="item">
-         <img src="https://productoraudiovisualbarcelona.com/wp-content/uploads/2018/02/fotografia-eventos-exposiciones-785x510.jpg" alt="New York" style="width: 600px;">
-        </div>
-        </div>
-
-  <!-- Left and right controls -->
-  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#myCarousel" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right"></span>
-    <span class="sr-only">Next</span>
-  </a>
-            </div>
-            
-        </div>
-        <div  class="child" style="width: 33%">
-          <p>haaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaola</p>
+          <h1>!El periodico pierde fuerza!</h1>
+          <p>La fotografía es testigo de la historia. Al hombre que detuvo por unos segundos una columna de tanques en la plaza de Tiannanen en Pequín se le llegó a llamar tank man (hombre tanque) o rebelde desconocido. Supuestamente era uno de los millares de estudiantes que se manifestó entre abril y junio de 1989 contra el gobierno de la República Popular China y cuyo movimiento acabó en una sangrienta represión por parte del Ejército Popular de Liberación. En abril de 1998 la revista Time consideró al rebelde desconocido como una de las personas más influyentes del siglo XX. Nada se sabe de él, muchos creen que fue ejecutado por su osadía, aunque ninguna información ha confirmado nunca esta teoría. Sea como fuere, jamás ha salido a la luz en busca de reconocimiento. Su gesto, en cambio, difícilmente será olvidado.</p>
         </div>
 
 </template>
@@ -105,5 +43,10 @@ Las imágenes obtenidas así también se denominan fotografías o fotos, y son e
   padding: 1rem 1rem;
   vertical-align: middle;
   color: black;
+  font-family: 'Times New Roman', Times, serif;
+}
+
+.titular{
+  font-weight: bold;
 }
 </style>
